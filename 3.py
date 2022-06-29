6e24d22bdd12095d81cd45974cbedff0e5d534c4
Herman70602 <108292371+Herman70602@users.noreply.github.com>
2022-06-29 21:56:13 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+character_name = "Henry"
+character_age = "37"
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old")
+character_name = "Eduard"
+character_age = "25"
+print("Man who called " + character_name + "was a moron which flunked the exam ")
+print("he was " + character_age + " years old")
