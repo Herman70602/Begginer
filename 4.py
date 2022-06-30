cb2c0607ddb707450ee6ce53d547976c780698af
Herman70602 <108292371+Herman70602@users.noreply.github.com>
2022-06-30 19:34:42 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,7 @@
+phase = "twenty one tree"
+print(phase.lower())
+print(phase.upper())
+print(len(phase))
+print(phase[7])
+print(phase.index("n"))
+print(phase.replace("twenty","fifty"))
---
+++ 5.py
@@ -0,0 +1,14 @@
+
+print(-2.00988)
+print(3+4.7)
+print(4 * 9+5)
+print(10%3)
+php = -16
+print(str(php + 18) +" feramop")
+print(abs(php))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4,11))
+print(round(3.5))
+from math import *
+print(sqrt(49) + pow(4, 3))
