7b46244d2bf3c6a0691353775c0e07f268b70254
Herman70602 <108292371+Herman70602@users.noreply.github.com>
2022-07-02 22:32:24 +0300

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,5 @@
+def say_hi(name, age):
+    print("Hello" + name + ", you are " + str(age))
+
+say_hi("Mike", 35)
+say_hi("Steve",  70)
---
+++ 13.py
@@ -0,0 +1,8 @@
+
+def cube(num):
+    return num*num*num
+
+
+
+result = cube(4)
+print(rusult)
